a490b9290dd0f9156f2eb8098839952360469a74
Ronny Wieckardt <ronny@ckdt.nl>
2015-02-23 15:15:52 +0100

    Wordpress Ready gemaakt
    source files naar /lib verplaatst en files aangepast zodat er straks
    een wordpress theme structuur uit komt al je build

---
+++ app/js/main.js
@@ -0,0 +1,25 @@
+ $(document).ready(function() {
+
+ 	// User Agent Variables
+	var isPhone = false;
+	var isIpad = false;
+	
+	// Detect User Agent
+	if(navigator.userAgent.match(/Android/i) ||
+		navigator.userAgent.match(/webOS/i) ||
+		navigator.userAgent.match(/iPhone/i) ||
+		navigator.userAgent.match(/iPod/i)) {
+		$('body').addClass('phone');
+		isPhone = true;
+	}
+	if(navigator.userAgent.match(/iPad/i)){
+		$('body').addClass('ipad');
+		isIpad = true;
+	}
+
+	//Fullpage JS
+    $('#fullpage').fullpage({
+        navigation: true,
+        verticalCentered: false
+    });
+});
---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage New_Business
+ * @since New Business 0.0.1
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/vendors/html5.min.js"></script>
+	<![endif]-->
+	<script>(function(){document.documentElement.className='js'})();</script>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
