d4373a935ad5baa786c64f373dde5289978f481e
Ronny Wieckardt <ronny@ckdt.nl>
2015-03-12 15:22:53 +0100

    Relations zijn gemaakt

---
+++ app/templates/slide-relation.php
@@ -0,0 +1,40 @@
+<?php $rel = get_sub_field( 'default_relation' ); ?>
+
+<?php if ( $slides = get_field( 'slides', $rel[0] ) ): ?>
+
+	<?php if ( ! post_password_required() ): ?>
+	
+		<?php while ( has_sub_field( 'slides', $rel[0]  ) ): ?>
+			<?php if ( get_row_layout() == 'chapter' ): // layout: Chapter ?>
+				<?php get_template_part( 'templates/slide', 'chapter' ); ?>
+			<?php endif; ?>
+			<?php if ( get_row_layout() == 'video' ): // layout: Video ?>
+				<?php get_template_part( 'templates/slide', 'video' ); ?>
+			<?php endif; ?>
+			<?php if ( get_row_layout() == 'text' ): // layout: Text ?>
+				<?php get_template_part( 'templates/slide', 'text' ); ?>
+			<?php endif; ?>
+			<?php if ( get_row_layout() == 'image_presenter' ): // layout: Image Presenter ?>
+				<?php get_template_part( 'templates/slide', 'presenter' ); ?>
+			<?php endif; ?>
+			<?php if ( get_row_layout() == 'default' ): // layout: Default ?>
+				<?php get_template_part( 'templates/slide', 'relation' ); ?>
+			<?php endif; ?>
+			<?php if ( get_row_layout() == 'image_grid' ): // layout: Image Grid ?>
+				<?php get_template_part( 'templates/slide', 'grid' ); ?>
+			<?php endif; ?>
+			<?php if ( get_row_layout() == 'overview' ): // layout: Overview ?>
+				<?php get_template_part( 'templates/slide', 'overview' ); ?>
+			<?php endif; ?>
+			<?php if ( get_row_layout() == 'estimate' ): // layout: Estimate ?>
+				<?php get_template_part( 'templates/slide', 'estimate' ); ?>
+			<?php endif; ?>
+		<?php endwhile; ?>
+		
+	<?php else: ?>
+		<?php get_template_part( 'templates/slide', 'password' ); ?>
+	<?php endif;?>
+
+<?php else: ?>
+	<?php get_template_part( 'templates/slide', '404' ); ?>
+<?php endif; ?>
